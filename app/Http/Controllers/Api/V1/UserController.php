cdc44df7a944a30e63aa19cbabba10266047616c
carobte <caro.bustamante.escobar@gmail.com>
2024-11-12 07:31:07 -0500

    feat: index users

---
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\isEmpty;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        if ($users->isEmpty()) {
+            // 204 -> No content
+            return response()->json([], 204);
+        }
+
+        return response()->json(array('users' => $users), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
